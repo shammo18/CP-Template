746b232d2b585cbbbe89bc4776f11995ee88493a
Khan Mohammad Borkotullah Shammo <65932871+shammo18@users.noreply.github.com>
2021-01-26 01:18:09 +0600

    Add files via upload

---
+++ String Processing/KMP lps or pi array implementation.cpp
@@ -0,0 +1,13 @@
+//pi[i] means longest prefix that is also a suffix ending at i
+// trick t + "#" + s 
+
+vector<int> prefix_function (string s) {
+	int n = (int) s.length(); vector<int> pi (n);
+	for (int i=1; i<n; ++i) {
+		int j = pi[i-1];
+		while (j > 0 && s[i] != s[j]) j = pi[j-1];
+		if (s[i] == s[j])  ++j;
+		pi[i] = j;
+	}
+	return pi;
+}
