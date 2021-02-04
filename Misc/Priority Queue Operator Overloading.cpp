3a4d77df4322e5930c335004dfb77f1c3c09f67c
Khan Mohammad Borkotullah Shammo <65932871+shammo18@users.noreply.github.com>
2021-02-04 21:24:26 +0600

    Update Priority Queue Operator Overloading.cpp

--- Misc/Priority Queue Operator Overloading.cpp
+++ Misc/Priority Queue Operator Overloading.cpp
@@ -17,3 +17,15 @@ struct Node{
 Node x(7 , 9 , 9); // declaring a Node
 
 priority_queue<Node> pq;
+
+// pointer stuff---------------------------------------
+
+  auto cmp = [] (Node* lhs, Node* rhs) noexcept -> bool {
+    return lhs->mn_len > rhs->mn_len;
+       // if return true then pq will swap them otherwise not
+       // here Node& a  is the 2nd node
+  };
+   
+  std::priority_queue<Node *, std::vector<Node *>, decltype(cmp)> pq(cmp);
+
+
