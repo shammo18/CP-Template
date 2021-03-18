bf7c44f971e3de4f6587d9ef405c641b92418559
Khan Mohammad Borkotullah Shammo <65932871+shammo18@users.noreply.github.com>
2021-03-18 19:46:12 +0600

    Update CRT.cpp

--- Math/CRT.cpp
+++ Math/CRT.cpp
@@ -1,5 +1,5 @@
-x = a1 mod m1
-x = a2 mod m2
+x = a1 (mod m1)
+x = a2 (mod m2)
 
 find x mod m1m2 ?
   
@@ -10,11 +10,11 @@ where ,
       solve this with egcd to find out p and q.
 
 For multiple equation find this one by one.
-x = a1 mod m1
-x = a2 mod m2
-x = a3 mod m3
+x = a1 (mod m1)
+x = a2 (mod m2)
+x = a3 (mod m3)
 .
 .
 .
-x = an mod mn
+x = an (mod mn)
 
