89cf7830be4b348c73552a1780843e8cc731ff6e
Khan Mohammad Borkotullah Shammo <65932871+shammo18@users.noreply.github.com>
2022-05-24 23:24:59 +0600

    Update MatExpo.cpp

--- Math/MatExpo.cpp
+++ Math/MatExpo.cpp
@@ -1,35 +1,94 @@
+#include<bits/stdc++.h>
+#define IOS ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 
-const int mod = 1e9 + 7;
+using namespace std;
+
+int mod;
+
 struct Mat {
-    int a[10][10], d;
-    Mat(int _d = 0) {
-        d = _d;
-        memset(a, 0, sizeof a);
+  int n, m;
+  vector<vector<int>> a;
+  Mat() { }
+  Mat(int _n, int _m) {n = _n; m = _m; a.assign(n, vector<int>(m, 0)); }
+  Mat(vector< vector<int> > v) { n = v.size(); m = n ? v[0].size() : 0; a = v; }
+  inline void make_unit() {
+    assert(n == m);
+    for (int i = 0; i < n; i++)  {n
+      for (int j = 0; j < n; j++) a[i][j] = i == j;
     }
-    void setIdentity() {
-        for(int i = 0; i < d; ++i)
-            for(int j = 0; j < d; ++j)
-                a[i][j] = (i == j);
+  }
+  inline Mat operator + (const Mat &b) {
+    assert(n == b.n && m == b.m);
+    Mat ans = Mat(n, m);
+    for(int i = 0; i < n; i++) {
+      for(int j = 0; j < m; j++) {
+        ans.a[i][j] = (a[i][j] + b.a[i][j]) % mod;
+      }
     }
-    Mat operator *(const Mat &p) const {
-        Mat ret(d); int i, j, k;
-        ll y = (ll)mod * mod, x;
-        for(i = 0; i < d; ++i) {
-            for(j = 0; j < d; ++j) {
-                for(k = x = 0; k < d; ++k) {
-                    x += (ll)a[i][k] * p.a[k][j];
-                    if(x >= y) x -= y;
-                } ret.a[i][j] = x % mod;
-            }
-        } return ret;
+    return ans;
+  }
+  inline Mat operator - (const Mat &b) {
+    assert(n == b.n && m == b.m);
+    Mat ans = Mat(n, m);
+    for(int i = 0; i < n; i++) {
+      for(int j = 0; j < m; j++) {
+        ans.a[i][j] = (a[i][j] - b.a[i][j] + mod) % mod;
+      }
     }
-    Mat pow(ll n) {
-        Mat ret(d); ret.setIdentity();
-        Mat b = *this;
-        for(; n; n >>= 1, b = b * b)
-            if(n & 1) ret = ret * b;
-        return ret;
+    return ans;
+  }
+  inline Mat operator * (const Mat &b) {
+    assert(m == b.n);
+    Mat ans = Mat(n, b.m);
+    for(int i = 0; i < n; i++) {
+      for(int j = 0; j < b.m; j++) {
+        for(int k = 0; k < m; k++) {
+          ans.a[i][j] = (ans.a[i][j] + 1LL * a[i][k] * b.a[k][j] % mod) % mod;
+        }
+      }
     }
+    return ans;
+  }
+  inline Mat pow(long long k) {
+    assert(n == m);
+    Mat ans(n, n), t = a; ans.make_unit();
+    while (k) {
+      if (k & 1) ans = ans * t;
+      t = t * t;
+      k >>= 1;
+    }
+    return ans;
+  }
+  inline Mat& operator += (const Mat& b) { return *this = (*this) + b; }
+  inline Mat& operator -= (const Mat& b) { return *this = (*this) - b; }
+  inline Mat& operator *= (const Mat& b) { return *this = (*this) * b; }
+  inline bool operator == (const Mat& b) { return a == b.a; }
+  inline bool operator != (const Mat& b) { return a != b.a; }
 };
 
+int main() {
+    IOS;
+    int t = 1;
+    cin >> t;
+    for(int ca = 1;ca <= t;ca++) {
+        cout << "Case " << ca << ": ";
+        long long a , b , n , m;
+        cin >> a >> b >> n >> m;
+        mod = pow(10 , m);
+        Mat A(2 , 2);
+        A.a[0][0] = 1;
+        A.a[0][1] = 1;
+        A.a[1][0] = 1;
+        A.a[1][1] = 0;
 
+        Mat B(2 , 1);
+        B.a[0][0] = b;
+        B.a[1][0] = a;
+
+        Mat C = A.pow(n - 1);
+        C = C * B;
+        cout << C.a[0][0] << endl;
+
+    }
+return 0;
+}
