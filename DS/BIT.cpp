1218d781f264926b40aa66e0ad8522e51604d1e8
Khan Mohammad Borkotullah Shammo <65932871+shammo18@users.noreply.github.com>
2022-05-24 20:51:55 +0600

    Update BIT.cpp

--- DS/BIT.cpp
+++ DS/BIT.cpp
@@ -1,70 +1,47 @@
-/******************************************************************/
-/*
-    *0-based index
-    *Fenwick(int n,T a[]) builds fenwick tree in O(n) from a[]
-    *query(int r) for range [0,r)
-    *query(int l,int r) for range [l,r)
-*/
-template<class T> struct Fenwick {
-    int n;
-    T* d;
-    Fenwick() : n(0), d(NULL) {}
-    Fenwick(int n) : n(n) {
-        d = new T[n]();
+//Tested : LightOJ 1164 - Horrible Queries
+// 1-indexed
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+const int MAXN = 1000005;
+
+ll BIT[2][MAXN];
+void update(int cs, int indx, ll val){
+    while(indx < MAXN){
+        BIT[cs][indx] += val;
+        indx += (indx & -indx);
     }
-    Fenwick(const Fenwick &y) : n(y.n) {
-        d = new T[n];
-        memcpy(d, y.d, sizeof (T) * n);
+}
+
+ll sum(int cs, int indx){
+    ll ans = 0;
+    while(indx != 0) {
+        ans += BIT[cs][indx];
+        indx -= (indx & -indx);
     }
-    Fenwick(int n, T a[]) : n(n){
-        d = new T[n]();
-        for(int i=0; i<n; i++){
-            d[i] += a[i];
-            if( (i|(i+1)) < n)
-                d[i | (i + 1)] += d[i];
+    return ans;
+}
+
+void updateRange(int l, int r, ll val){
+    update(0,l,val);       update(0,r+1,-val);
+    update(1,l,val*(l-1)); update(1,r+1,-val*r);
+}
+
+ll sumRange(int indx) {return sum(0,indx)*indx - sum(1,indx);}
+ll QueryRange(int l, int r) {return sumRange(r)-sumRange(l-1);}
+
+const int LOGN = 20;
+int LowerBound(int cs, ll v){
+    ll sum = 0;
+    int indx = 0;
+    for(int i = LOGN; i >= 0; i--){
+        int nPos = indx + (1<<i);
+        if(nPos < MAXN && sum + BIT[cs][nPos] < v){
+            sum += BIT[cs][nPos];
+            indx = nPos;
         }
     }
-    ~Fenwick() {
-        delete[] d; d = NULL;
-        n = 0;
-    }
-    inline void update(int i, const T &x) {
-        for (; i<n; i|=i+1) d[i] += x;
-    }
-    inline T query(int r) {
-        T s = T();
-        for (; r; r&=r-1) s += d[r-1];
-        return s;
-    }
-    T query(int l, int r) {
-        return query(r) - query(l);
-    }
-};
-
-/// into main function
-/// # of distinct number in a range l.....r (offline solution with BIT)
-    int q;
-    cin >> q;
-
-    for(int i = 0;i < q;i++){
-        int l , r;
-        cin >> l >> r;
-        l--; r--;
-        qr[r].pb({l , i});
-    }
-
-    int sz = arr.size();
-    Fenwick<int> bit(sz + 1);
-    vector<int> last_vis(ptr + 10 , -1);
-    vector<int> ans(q + 5);
-
-    for(int i = 0;i < sz;i++){
-        if(last_vis[arr[i]] != -1) {
-            bit.update(last_vis[arr[i]] , -1);
-        }
-        last_vis[arr[i]] = i;
-        bit.update(i , 1);
-        for(auto u : qr[i]) ans[u.yy] = bit.query(u.xx , i + 1);
-    }
-
-    for(int i = 0;i < q;i++) cout << ans[i] << endl;
+    //pos = maximal x such that Sum(x) < v
+    return indx + 1; //+1 for LowerBound
+}
